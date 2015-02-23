a10333f94fe26c41d7675feb85fff3f8831c26a8
Stephan Fowler <stephanfowler@gmail.com>
2015-02-23 23:21:00 +0000

    Snapshotter uses order-change detection

--- snapshotter/app.js
+++ snapshotter/app.js
@@ -5,22 +5,20 @@ var http = require('http'),
     count = 0,
     prev;
 
-    //url = 'http://content.guardianapis.com/search?api-key=test&show-tags=all&order-by=newest&use-date=last-modified&page-size=10&callback=jsonpFn';
-    url = 'http://content.guardianapis.com/search?api-key=test&show-tags=all&order-by=newest&page-size=10&callback=jsonpFn';
+    url = 'http://content.guardianapis.com/search?api-key=test&show-tags=all&order-by=newest&use-date=last-modified&page-size=10&callback=jsonpFn';
+    //url = 'http://content.guardianapis.com/search?api-key=test&show-tags=all&order-by=newest&page-size=10&callback=jsonpFn';
 
 function save(index, data) {
     fs.writeFile("data/" + index, data, function(err) {
         if(err) {
             console.log(err);
         } else {
-            console.log("File was saved: " + index);
+            console.log("Snapshot saved: " + index);
         }
     });     
 }
 
 function fetch() {
-    console.log('Fetching...');
-
     http.get(url, function(res) {
         var body = '';
 
@@ -29,14 +27,20 @@ function fetch() {
         });
 
         res.on('end', function() {
-            if (prev !== body) {
-                prev = body;
+            var json = (body || '').replace(/^jsonpFn\(/, '').replace(/\)$/, '');
+                
+            json = JSON.stringify(((JSON.parse(json) || {}).response || {}).results.map(function(a) { return a.id }));
+
+            if (json && prev !== json) {
+                prev = json;
                 save(max + count, body);
                 count += 1;
+            } else {
+                console.log('No change...')
             }
 
             if (count < max) {
-                setTimeout(fetch, 10000)
+                setTimeout(fetch, 5000)
             }
         });
     }).on('error', function(e) {
