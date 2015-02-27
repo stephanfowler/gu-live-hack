1495872c60b4d424aa617075699af6aee50dc8f1
Stephan Fowler <stephanfowler@gmail.com>
2015-02-27 13:15:41 +0000

    Two chrome extensions

--- chrome-extension-snapshots/inject-iframe.js
+++ chrome-extension-snapshots/inject-iframe.js
@@ -1,10 +1,59 @@
 console.log('Loading live newsdesk... SNAPSHOTS');
 
-var articleEl = document.querySelector('.content__secondary-column .js-mpu-ad-slot');
-    frontEl = document.querySelectorAll('.fc-slice__item');
+var url = 'http://stephanfowler.com/gu-live-hack/#snapshots',
+
+    articleEl = document.querySelector('.content__secondary-column .js-mpu-ad-slot'),
+    frontEl = document.querySelectorAll('.fc-slice__item'),
+
+    editionalised = [
+        'business',
+        'commentisfree',
+        'culture',
+        'environment',
+        'media',
+        'money',
+        'sport',
+        'technology'
+    ],
+
+    editions = [
+        'uk',
+        'us',
+        'au'
+    ],
+
+    pathLength,
+
+    isEditionalisedRx = new RegExp('^(' + editions.join('|') + ')\/(' + editionalised.join('|') + ')$'),
+    stripEditionRx = new RegExp('^(' + editions.join('|') + ')\/');
+
+function collapsePath(t) {
+    if (t) {
+        t = t.replace(/^\/|\/$/g, '');
+        if (t.match(isEditionalisedRx)) {
+            t = t.replace(stripEditionRx, '');
+        }
+        t = t.split('/');
+        t = t.length === 2 && t[0] === t[1] ? [t[0]] : t;
+        return t.join('/');
+    } else {
+        return '';
+    }
+}
+
+pathLength = collapsePath(window.location.pathname).split('/').length;
+
+if (collapsePath(window.location.pathname) === 'uk') {
+    url += '&header=Election%20newsdesk';
+
+} else if (pathLength < 3) {
+    url += '&header=' + collapsePath(window.location.pathname) + '%20newsdesk';
+}
+
+console.log(url);
 
 if (articleEl) {
-    articleEl.insertAdjacentHTML('beforebegin', '<iframe src="http://stephanfowler.com/gu-live-hack/#snapshots" style="border: 0; float:  left; clear: left; width: 100%; height: 300px; margin: 0 0 72px 0;"></iframe>')
+    articleEl.insertAdjacentHTML('beforebegin', '<iframe src="' + url + '" style="border: 0; float:  left; clear: left; width: 100%; height: 300px; margin: 0 0 72px 0;"></iframe>')
 }
 
 else if (frontEl && frontEl[1]) {
