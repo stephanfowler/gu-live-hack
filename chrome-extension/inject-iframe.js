7766da9fd54bb39cf2738a503f63945d22a70987
Stephan Fowler <stephanfowler@gmail.com>
2015-03-02 15:30:28 +0000

    Use query not hash

--- chrome-extension/inject-iframe.js
+++ chrome-extension/inject-iframe.js
@@ -1,12 +1,14 @@
 console.log('Loading live newsdesk... SNAPSHOTS');
 
-var useSnapshots = false,
+var useSnapshots = true,
     
     url = 'http://stephanfowler.com/gu-live-hack/',
     articleEl = document.querySelector('.content__secondary-column .js-mpu-ad-slot'),
     frontEl = document.querySelectorAll('.fc-slice__item');
 
-url += useSnapshots ? '#snapshots' : '';
+url += useSnapshots ? '?snapshots' : '';
+
+console.log('URl: ' + url);
 
 if (articleEl) {
     articleEl.insertAdjacentHTML('beforebegin', '<iframe src="' + url + '" style="border: 0; float:  left; clear: left; width: 100%; height: 300px; margin: 0 0 72px 0;"></iframe>')
